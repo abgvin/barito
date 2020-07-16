071cffabfbbd1e45db4f5b582d6f7e43c4f2114f
abgvin <muhamadapri10@gmail.com>
2020-07-16 11:27:27 +0700

    Perbaikan bagian pushManager

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,22 +40,24 @@
          return;
        }
 
-       if (('PushManager' in window)) {
-         navigator.serviceWorker.getRegistration().then(function (registration) {
-           registration.pushManager.subscribe({
-             userVisibleOnly: true,
-             applicationServerKey: urlBase64ToUint8Array("BPYJuW7C2VkGOuZXkac9RHXvogzSxqYeYLPeqzFMioyzyH2IwZX_56BldGRm8r4dD1VcULyLBwnmOJyvWRBQ5n8")
-           }).then(function (subscribe) {
-             console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint);
-             console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
-               null, new Uint8Array(subscribe.getKey('p256dh')))));
-             console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
-               null, new Uint8Array(subscribe.getKey('auth')))));
-           }).catch(function (e) {
-             console.error('Tidak dapat melakukan subscribe ', e.message);
+       navigator.serviceWorker.ready.then(function () {
+         if (('PushManager' in window)) {
+           navigator.serviceWorker.getRegistration().then(function (registration) {
+             registration.pushManager.subscribe({
+               userVisibleOnly: true,
+               applicationServerKey: urlBase64ToUint8Array("BPYJuW7C2VkGOuZXkac9RHXvogzSxqYeYLPeqzFMioyzyH2IwZX_56BldGRm8r4dD1VcULyLBwnmOJyvWRBQ5n8")
+             }).then(function (subscribe) {
+               console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint);
+               console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
+                 null, new Uint8Array(subscribe.getKey('p256dh')))));
+               console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
+                 null, new Uint8Array(subscribe.getKey('auth')))));
+             }).catch(function (e) {
+               console.error('Tidak dapat melakukan subscribe ', e.message);
+             });
            });
-         });
-       }
+         }
+       })
      });
    }
  }
